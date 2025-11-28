003a567cb5fae12103b58797cbf4970a93f1b621
Pybounce <pybounce@protonmail.com>
2025-11-28 15:59:27 +0000

    memory leaking strings yay

--- debug.c
+++ debug.c
@@ -62,7 +62,6 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_NOT", offset);
         case OP_NEGATE:
             return simpleInstruction("OP_NEGATE", offset);
-
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         default:
